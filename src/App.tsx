dc41dfdb68a5608b9d8a20d2d410be6e54108987
youngmentor <olaleresuliton43@gmail.com>
2025-03-07 15:03:35 +0100

    nice changes

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,26 @@
 import React from 'react';
-import { Routes, Route, HashRouter } from 'react-router-dom';
+import { Routes, Route, BrowserRouter } from 'react-router-dom';
 import CreateMeeting from './pages/createMeeting';
-import MeetingsList from './pages/meetingList';
+// import MeetingsList from './pages/meetingList';
 import AddParticipants from './pages/addParticipants';
 import MeetingDetails from './pages/meetingDetails';
 import ParticipantsList from './pages/spinParticipants';
+import FileUploadSpinner from './NameSpinner';
 // import FileUploadSpinner from './NameSpinner';
 
 
 const App: React.FC = () => {
   return (
     <div>
-      <HashRouter>
+      <BrowserRouter>
         <Routes>
           <Route path='/create-meeting' element={<CreateMeeting />} />
-          <Route path='/' element={<MeetingsList />} />
+          <Route path='/' element={<FileUploadSpinner />} />
           <Route path='/meetings/:meetingId' element={<MeetingDetails />} />
           <Route path='/meetings/:id/add-participants' element={<AddParticipants />} />
           <Route path='/meetings/:meetingId/spin-participants' element={<ParticipantsList />} />
         </Routes>
-      </HashRouter>
+      </BrowserRouter>
     </div>
   );
 };

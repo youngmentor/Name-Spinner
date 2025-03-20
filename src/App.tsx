255dce2a078e69b42f8b39f24b785b7e9b810948
youngmentor <olaleresuliton43@gmail.com>
2025-03-20 10:58:52 +0100

    refactor routing: switch from BrowserRouter to HashRouter and update route paths for clarity

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,25 @@
 import React from 'react';
-import { Routes, Route, BrowserRouter } from 'react-router-dom';
+import { Routes, Route, HashRouter } from 'react-router-dom';
 import CreateMeeting from './pages/createMeeting';
-// import MeetingsList from './pages/meetingList';
+import MeetingsList from './pages/meetingList';
 import AddParticipants from './pages/addParticipants';
 import MeetingDetails from './pages/meetingDetails';
 import ParticipantsList from './pages/spinParticipants';
-import { Toaster } from 'react-hot-toast';
 // import FileUploadSpinner from './NameSpinner';
 
 
 const App: React.FC = () => {
   return (
     <div>
-      <BrowserRouter>
+      <HashRouter>
         <Routes>
-          <Route path='/create-meeting' element={<CreateMeeting />} />
+          <Route path='/' element={<CreateMeeting />} />
+          <Route path='/meetings' element={<MeetingsList />} />
           <Route path='/meetings/:meetingId' element={<MeetingDetails />} />
           <Route path='/meetings/:id/add-participants' element={<AddParticipants />} />
           <Route path='/meetings/:meetingId/spin-participants' element={<ParticipantsList />} />
         </Routes>
-        <Toaster />
-      </BrowserRouter>
+      </HashRouter>
     </div>
   );
 };

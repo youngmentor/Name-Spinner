f19be383ab188d09afff894847ff755b491316d9
youngmentor <olaleresuliton43@gmail.com>
2025-03-20 10:56:55 +0100

    refactor App component: remove unused FileUploadSpinner import and clean up routing structure

--- src/App.tsx
+++ src/App.tsx
@@ -5,11 +5,7 @@ import CreateMeeting from './pages/createMeeting';
 import AddParticipants from './pages/addParticipants';
 import MeetingDetails from './pages/meetingDetails';
 import ParticipantsList from './pages/spinParticipants';
-<<<<<<< HEAD
 import { Toaster } from 'react-hot-toast';
-=======
-import FileUploadSpinner from './NameSpinner';
->>>>>>> dc41dfdb68a5608b9d8a20d2d410be6e54108987
 // import FileUploadSpinner from './NameSpinner';
 
 
@@ -19,17 +15,12 @@ const App: React.FC = () => {
       <BrowserRouter>
         <Routes>
           <Route path='/create-meeting' element={<CreateMeeting />} />
-          <Route path='/' element={<FileUploadSpinner />} />
           <Route path='/meetings/:meetingId' element={<MeetingDetails />} />
           <Route path='/meetings/:id/add-participants' element={<AddParticipants />} />
           <Route path='/meetings/:meetingId/spin-participants' element={<ParticipantsList />} />
         </Routes>
-<<<<<<< HEAD
-      </HashRouter>
-      <Toaster/>
-=======
+        <Toaster />
       </BrowserRouter>
->>>>>>> dc41dfdb68a5608b9d8a20d2d410be6e54108987
     </div>
   );
 };
